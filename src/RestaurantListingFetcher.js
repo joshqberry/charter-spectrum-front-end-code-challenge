fbd5530b51672e5cf328c38663d0ee1783c2afcd
Josh Qberry <joshqberry@gmail.com>
2020-10-28 20:33:57 -0600

    adds table labels

--- src/RestaurantListingFetcher.js
+++ src/RestaurantListingFetcher.js
@@ -23,65 +23,52 @@ import App from "./App.css"
 
     }, []); // ends useEffect
 
-  return (
+    return (
       <div className="data-container">
-        <h1>These are the listings</h1>
         <ul>
-        {
-          listings.map((listing, index) => {
-            return ( <div
-              index={index}>
-              <ul>
-                   <li className="restaurantName">
-                    <h5>{listing.name}</h5>
-                   </li>
-                   <li className="restaurantCity">
-                     <h5>{listing.city}</h5>
-                   </li>
-                   <li className="restaurantState">
-                     <h4>{listing.state}</h4>
-                   </li>
-                   <li className="restaurantTelephone">
-                     <p>{listing.telephone}</p>
-                   </li>
-                   <li className="restaurantGenre">
-                     <p>{listing.genre}</p>
-                   </li>
-               </ul>
-            </div> )
-          }) //ends map function
-        } 
+          <li className="tableHeader">
+            <h2>Restaurant name</h2>
+          </li>
+          <li className="tableHeader">
+            <h2>City</h2>
+          </li>
+          <li className="tableHeader">
+            <h2>State</h2>
+          </li>
+          <li className="tableHeader">
+            <h2>Telephone number</h2>
+          </li>
+          <li className="tableHeader">
+            <h2>Genre</h2>
+          </li>
+        </ul>
+        <ul>
+          {listings.map((listing, index) => {
+            return (
+              <div index={index}>
+                <ul>
+                  <li className="restaurantName">
+                    <h4>{listing.name}</h4>
+                 </li>
+                 <li className="restaurantCity">
+                    <h4>{listing.city}</h4>
+                  </li>
+                  <li className="restaurantState">
+                    <h4>{listing.state}</h4>
+                  </li>
+                  <li className="restaurantTelephone">
+                    <h4>{listing.telephone}</h4>
+                  </li>
+                  <li className="restaurantGenre">
+                    <h4>{listing.genre}</h4>
+                  </li>
+                </ul>
+              </div> )
+            }) //ends map function
+          }
         </ul>
       </div>
     ); // ends return
   }; // end RestaurantListingFetcher function
-
-
-  const Resty = ({ name, city, state, telephone, genre }) => {
-  if (!name) return <div />;
-  return (
-    <div>
-       <ul>
-            <li className="restaurantName">
-             <h5>{name}</h5>
-            </li>
-            <li className="restaurantCity">
-              <h5>{city}</h5>
-            </li>
-            <li className="restaurantState">
-              <h4>{state}</h4>
-            </li>
-            <li className="restaurantTelephone">
-              <p>{telephone}</p>
-            </li>
-            <li className="restaurantGenre">
-              <p>{genre}</p>
-            </li>
-        </ul>
-      </div>
-    );
-  };
-
-
 
 export { RestaurantListingFetcher };
